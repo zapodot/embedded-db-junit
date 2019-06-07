8377d10eca09e9cad0e4d400b48fd8159ddb8bce
Sondre Eikanger Kvalø <zapodot@gmail.com>
2019-06-07 08:49:39 +0200

    Upgrade h2 to v.1.4.199 to resolve #29

--- embedded-db-junit/src/test/java/org/zapodot/junit/db/EmbeddedDatabaseRuleMultipleTest.java
+++ embedded-db-junit/src/test/java/org/zapodot/junit/db/EmbeddedDatabaseRuleMultipleTest.java
@@ -1,10 +1,12 @@
 package org.zapodot.junit.db;
 
 import com.github.davidmoten.rx.jdbc.Database;
+import org.h2.api.TimestampWithTimeZone;
 import org.junit.Rule;
 import org.junit.Test;
 import rx.Observable;
 
+import java.time.Instant;
 import java.util.Date;
 
 import static org.junit.Assert.assertTrue;
@@ -21,12 +23,12 @@ public class EmbeddedDatabaseRuleMultipleTest {
             EmbeddedDatabaseRule.builder().h2().withName("db2").withMode(CompatibilityMode.MSSQLServer).build();
 
     @Test
-    public void testStuff() throws Exception {
+    public void testStuff() {
         final Database mysql = Database.from(embeddedDatabaseMysqlRule.getConnection());
         final Database mssqlServer = Database.from(embeddedDatabaseMsSqlServerRule.getConnection());
 
-        final Observable<Date> dateMysql = mysql.select("select sysdate from dual").getAs(Date.class);
-        final Observable<Date> dateMssql = mssqlServer.select("select sysdate from dual").getAs(Date.class);
+        final Observable<TimestampWithTimeZone> dateMysql = mysql.select("select sysdate from dual").getAs(TimestampWithTimeZone.class);
+        final Observable<TimestampWithTimeZone> dateMssql = mssqlServer.select("select sysdate from dual").getAs(TimestampWithTimeZone.class);
 
         assertTrue(Observable.zip(dateMysql, dateMssql, (date, date2) -> date != null && date2 != null).toBlocking().single());
 
